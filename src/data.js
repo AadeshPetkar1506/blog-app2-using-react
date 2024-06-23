9b18c3cacccb0923456265e88449758b49fd2bb3
AadeshPetkar1506 <petkaraadesh15@gmail.com>
2024-06-24 00:39:58 +0530

    blog app created

---
+++ src/data.js
@@ -0,0 +1,42 @@
+const blog = [
+    {
+      id:"1",
+      title:"Black Molly",
+      content:"The Black Molly fish, with its sleek ebony body and lively demeanor, adds an elegant touch to any aquarium. These freshwater beauties are known for their peaceful nature and adaptability, making them a favorite among aquarists of all levels. Whether swimming gracefully or delicately exploring their surroundings, Black Molly fish bring a touch of charm and contrast to aquatic landscapes. Ideal for community tanks, they thrive in well-maintained environments with plenty of plants and hiding spots. With proper care, these fish can live long and healthy lives, delighting enthusiasts with their striking appearance and gentle disposition.",
+      image:"https://fishkeepingadvice.com/wp-content/uploads/Black_Molly-min.jpg",
+      author_name:"Poecilia sphenops", 
+      date:"June 18,1846"
+
+    },
+
+    {
+        id:" 2 ",
+        title:"Rosemary Barb ",
+        content:"The Rosemary Barb fish, named for its subtle rosy hues and graceful demeanor, is a captivating addition to freshwater aquariums. With its slender body and shimmering scales, this species displays a tranquil yet active nature, adding vibrancy to any aquatic setting. Known for their peaceful temperament, Rosemary Barbs thrive in schools, where they exhibit playful interactions and elegant swimming patterns. Their preference for well-planted tanks with ample swimming space makes them an ideal choice for community setups. Enthusiasts appreciate their beauty and easygoing nature, making the Rosemary Barb a beloved choice among aquarium hobbyists worldwide.",
+        image:"https://www.fishkeepingworld.com/wp-content/uploads/2019/11/Rosy-Barb-Tank-Setup-Care-Guide-Breeding-And-More%E2%80%A6-Banner.jpg",
+        author_name:"Alison Yang",
+        date:"June 21,2024"
+    },
+
+    
+    {
+        id:" 3 ",
+        title:"Cory Catfish",
+        content:"The Cory Catfish, also known as Corydoras, are beloved for their endearing personalities and unique behaviors in freshwater aquariums. With their adorable whisker-like barbels and armored bodies, Cory Catfish bring charm and activity to any tank. These social creatures are often seen darting playfully around the bottom of the aquarium, scavenging for food and interacting with tank mates. Their peaceful nature makes them excellent companions in community setups, where they help maintain cleanliness by consuming leftover food and debris. Enthusiasts admire their resilience and adaptability, ensuring a lively and harmonious aquatic environment.",
+        image:"https://www.fishlaboratory.com/wp-content/uploads/2022/01/Emerald-Cory-Catfish-2048x1366.jpeg",
+        author_name:"Millie Sheppard",
+        date:"June 21,2024"
+    },
+
+    
+    {
+        id:" 4  ",
+        title:"Platy fish",
+        content:"Platy fish are delightful additions to any freshwater aquarium, known for their vibrant colors and lively personalities. These small, peaceful fish come in a variety of hues, from bright oranges and yellows to subtle blues and greens, adding a splash of color to your tank. Platy fish are easy to care for and thrive in community setups, where they enjoy swimming together and exploring their surroundings. They are active swimmers, often seen darting around plants and decorations, bringing energy and movement to the aquarium. With their gentle nature and hardy disposition, platy fish are perfect for beginners and experienced aquarists alike, offering a captivating glimpse into the beauty of underwater life.",
+        image :"https://i.ytimg.com/vi/JEnxfKAZu38/maxresdefault.jpg",
+        author_name:"Oliver Jones",
+        date:"June 21,2024"
+    }
+]
+
+export default blog
